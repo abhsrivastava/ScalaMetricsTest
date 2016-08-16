e9f8b54ea6cb3ac2122e08f1b006cb2268943b34
Abhishek Srivastava <abhsrivastava@researchnow.com>
2016-08-16 12:21:42 -0500

    changed comments

--- src/main/scala-2.11/com/abhi/ServiceActor.scala
+++ src/main/scala-2.11/com/abhi/ServiceActor.scala
@@ -27,7 +27,7 @@ class ServiceActor extends Actor with HttpService with Instrumented {
             respondWithMediaType(`application/json`) {
                complete {
                   Future {
-                     Thread.sleep(1000) // fuck the server
+                     Thread.sleep(1000) // cause delay
                      println("going to return object")
                      Person("Bob", "Type A", System.currentTimeMillis())
                   }
@@ -44,7 +44,7 @@ class ServiceActor extends Actor with HttpService with Instrumented {
                complete {
                   loading.timeFuture {
                      Future {
-                        Thread.sleep(1000) // fuck the server
+                        Thread.sleep(1000) // cause delay
                         println("going to return instrumented object")
                         Person("Bob", "Type A", System.currentTimeMillis())
                      }
