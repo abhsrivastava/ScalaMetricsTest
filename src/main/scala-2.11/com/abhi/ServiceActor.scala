082f10769cf7bb5aaf89680fc988ad3e40eef9b4
Victor Reventos <victor.reventos1@gmail.com>
2016-08-16 14:27:45 -0500

    endpoint executor

--- src/main/scala-2.11/com/abhi/ServiceActor.scala
+++ src/main/scala-2.11/com/abhi/ServiceActor.scala
@@ -1,12 +1,14 @@
 package com.abhi
 
+import java.util.concurrent._
+
 import akka.actor.Actor
+import com.codahale.metrics.ScheduledReporter.NamedThreadFactory
 import spray.http.MediaTypes._
 import spray.json.DefaultJsonProtocol
 import spray.routing.HttpService
 
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
 
 
 case class Person (name: String, firstName: String, age: Long)
@@ -21,6 +23,13 @@ class ServiceActor extends Actor with HttpService with Instrumented {
    import MyJsonProtocol._
    def actorRefFactory = context
    def receive = runRoute(noInstrumentation ~ instrumentation)
+
+   implicit val endpointExecutor = {
+      val pool = new ThreadPoolExecutor(30, 100, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue[Runnable](), new NamedThreadFactory("endpoint-executor"))
+
+      ExecutionContext.fromExecutorService(pool)
+   }
+
    val noInstrumentation = {
       path("path1") {
          get {
